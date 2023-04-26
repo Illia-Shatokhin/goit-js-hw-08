bd9ead3e698e02d903112c1d24a2846b8a1d8249
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-26 15:05:17 +0300

    add marking and SimpleLightbox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,23 @@
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryList = document.querySelector('.gallery');
+galleryList.style.listStyle = 'none';
 
 const listItemString = galleryItems
   .map(
-    ({
-      preview,
-      original,
-      description,
-    }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
-class="gallery__image"
-src="${preview}"
-data-source="${original}"
-alt="${description}"
-/></a></li>`
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
   )
   .join('');
 
 galleryList.insertAdjacentHTML('afterbegin', listItemString);
 
-galleryList.addEventListener('click', openOriginalSize);
-
-function openOriginalSize(event) {
-  event.preventDefault();
-  if (event.target.nodeName === 'IMG') {
-    const original = event.target.getAttribute('data-source');
-    const instance = basicLightbox.create(`<img src="${original}">`);
-    instance.show();
-    document.addEventListener('keydown', function closeModalOnEsc(event) {
-      if (event.code === 'Escape') {
-        instance.close();
-        document.removeEventListener('keydown', closeModalOnEsc);
-      }
-    });
-  }
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'outside',
+});
