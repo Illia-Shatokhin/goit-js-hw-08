2eedf0c7097a5b0fc31b006eec7357c895b25e38
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-26 16:18:36 +0300

    console.log data from vimeo with 1000ms trottle from lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(data => console.log(data), 1000)
+);
+
+player.setCurrentTime(130.456);
