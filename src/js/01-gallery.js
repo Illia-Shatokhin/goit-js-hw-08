9898715422b88a16854eb4639d385aee516a60a0
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-26 14:55:21 +0300

    add old marking in 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const listItemString = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/></a></li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', listItemString);
+
+galleryList.addEventListener('click', openOriginalSize);
+
+function openOriginalSize(event) {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    const original = event.target.getAttribute('data-source');
+    const instance = basicLightbox.create(`<img src="${original}">`);
+    instance.show();
+    document.addEventListener('keydown', function closeModalOnEsc(event) {
+      if (event.code === 'Escape') {
+        instance.close();
+        document.removeEventListener('keydown', closeModalOnEsc);
+      }
+    });
+  }
+}
